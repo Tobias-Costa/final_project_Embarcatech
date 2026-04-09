906170594804963c26764608a2bcc81d0ba7a654
Tobias <tobiascosta064@gmail.com>
2026-04-09 20:13:41 -0300

    fix: qrcode.c directory error

--- libs/qrcode-master/qrcode-master.cmake
+++ libs/qrcode-master/qrcode-master.cmake
@@ -1,6 +1,6 @@
 # Define a biblioteca como uma "INTERFACE" ou "STATIC" library
 add_library(qrcode-master STATIC
-    ${PICO_SSD1306_DIR}/qrcode.c
+    ${QR_CODE_MASTER_DIR}/qrcode.c
 )
 
 # Diz ao CMake onde estão os arquivos .h (headers)
